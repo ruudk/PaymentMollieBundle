1cb0ed2bd9155148b8302852ec1557a01cff9bad
Pascal de Vink <pascal@ticketswap.com>
2016-07-25 16:45:56 +0200

    Set choices as values in iDeal form

--- src/Form/IdealType.php
+++ src/Form/IdealType.php
@@ -16,7 +16,6 @@ class IdealType extends AbstractType
     protected $issuers = array();
 
     /**
-     * @param string $name
      * @param array  $issuers
      */
     public function __construct(array $issuers)
@@ -31,15 +30,15 @@ class IdealType extends AbstractType
         $banks = array();
         $defaultBank = null;
         foreach($this->issuers AS $issuer) {
-            if('ideal' !== $issuer->getPaymentMethod()) {
+            if ('ideal' !== $issuer->getPaymentMethod()) {
                 continue;
             }
 
-            $banks[$issuer->getId()] = $issuer->getName();
+            $banks[$issuer->getName()] = $issuer->getId();
             $defaultBank = $issuer->getId();
         }
 
-        if(1 !== count($banks)) {
+        if (1 !== count($banks)) {
             $defaultBank = null;
         }
 
@@ -48,10 +47,11 @@ class IdealType extends AbstractType
         }
 
         $builder->add('bank', ChoiceType::class, array(
-            'label'       => 'ruudk_payment_mollie.ideal.bank.label',
-            'data'        => $defaultBank,
-            'empty_value' => 'ruudk_payment_mollie.ideal.bank.empty_value',
-            'choices'     => $banks
+            'label'             => 'ruudk_payment_mollie.ideal.bank.label',
+            'data'              => $defaultBank,
+            'empty_value'       => 'ruudk_payment_mollie.ideal.bank.empty_value',
+            'choices'           => $banks,
+            'choices_as_values' => true,
         ));
     }
 
